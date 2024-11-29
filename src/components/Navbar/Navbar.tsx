aff7f5c05977bc74b7e54037f69b12488b2b0135
speedy008 <kajal10395@gmail.com>
2024-11-29 14:19:12 +0530

    Colormode Implemented

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image, Text } from '@chakra-ui/react'
 import logo from '../../assets/Logo/logo.webp'
+import { ColorModeButton } from '../ui/color-mode'
 
 const Navbar = () => {
   return (
    <HStack>
     <Image src={logo} alt='logo' boxSize='60px'  />
     <Text>Navbar</Text>
+    <ColorModeButton  />
    </HStack>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.tsx";
-import theme from "./libs/theme.ts";
 import "./index.css";
 import App from "./App.tsx";
 import { ColorModeProvider } from "./components/ui/color-mode.tsx";
