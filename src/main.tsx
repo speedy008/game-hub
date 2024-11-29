4cf0cafc0a9293e55223826cdf85b433f40f5bfe
speedy008 <kajal10395@gmail.com>
2024-11-29 11:55:03 +0530

    Implement Dark Mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Provider } from "./components/ui/provider.tsx"
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "./components/ui/provider.tsx";
+import theme from "./libs/theme.ts";
+import "./index.css";
+import App from "./App.tsx";
+import { ColorModeProvider } from "./components/ui/color-mode.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider>
-    <App />
+      <ColorModeProvider forcedTheme="dark">
+        <App />
+      </ColorModeProvider>
     </Provider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
